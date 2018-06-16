18501f8f4afee0fb775a627c132a7bd4600f7471
livello <livello@bk.ru>
2018-06-16 23:59:14 +0300

    17.06 honey race

--- TagsRead.py
+++ TagsRead.py
@@ -1,6 +1,7 @@
 import serial
 import Settings
 
+f = open("tags.txt", "w")
 
 ser = serial.Serial(Settings.COMPortName, Settings.COMBaudRate)
 while(ser.is_open == True):
@@ -8,4 +9,9 @@ while(ser.is_open == True):
     incomingByte = ser.read(3)
     for i in incomingByte:
         rfidtag = rfidtag + str(i)
-    print(rfidtag)
+    try:
+        print(rfidtag+": "+str(Settings.TagsIntoNumbers[rfidtag]))
+    except:
+        number = input(rfidtag+": ")
+        if len(number)>0:
+            f.write("\"{0}\":{1},\n".format(rfidtag, number))
